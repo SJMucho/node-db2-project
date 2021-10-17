c66d2240b945ba2bfdabe7627b5ad25621fd7786
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-10-17 15:26:31 -0400

    table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,12 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable("cars", (tbl) => {
+    tbl.increments();
+    tbl.text("vin", 128).unique().notNullable();
+    tbl.text("make", 128);
+    tbl.text("model", 128);
+  });
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars");
 };
